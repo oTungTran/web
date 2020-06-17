ba483e94a06b34891a3353ef0d32aaab6f070f88
oTungTran <66772475+oTungTran@users.noreply.github.com>
2020-06-17 16:21:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
 	<meta charset="UTF-8">
 	<title>Login Page</title>
 </head>
-<body >
-<div class="start">
+<body>
 <form action="connect.php" method="POST" accept-charset="utf-8">
 	
-User <input type="text" name="username" value="" placeholder="username"/><br/>
+User<input type="text" name="username" value="" placeholder="username"/><br/>
 Password<input type="password" name="pass" value="" placeholder="**********"/><br/>
 <input type="submit" value="Login"/><br/>
 </form>
