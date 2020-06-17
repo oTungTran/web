647ef7aeda7d37cfa466e94ba0e78baa7153aa67
oTungTran <66772475+oTungTran@users.noreply.github.com>
2020-06-17 16:28:03 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -10,7 +10,7 @@ if (isset($_POST['pass'])) {
 	# code...
 	$pass = $_POST['pass'];
 }
-$sql = "SELECT * FROM tblAccount WHERE user='".$username."' AND pass'".$pass."'";
+$sql = "SELECT * FROM tblAccount WHERE user='".$username."' AND pass='".$pass."'";
 $result =pg_query($dbconn, $sql);
 $row = pg_num_rows($result);
 if($row==1)
